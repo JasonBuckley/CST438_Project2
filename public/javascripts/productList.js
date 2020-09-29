44401f005ec5d71a21c2a6a74f6309e9f8f08ca3
Jason Buckley <jbuckley@csumb.edu>
2020-09-29 00:14:43 -0700

    provided a path, and logic to get from the product list to a specific product

--- public/javascripts/productList.js
+++ public/javascripts/productList.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // grabs the products from the product api
     $.ajax({
-        url: "/product/",
+        url: "/product",
         method: "GET",
         dataType: "json",
         success: function (result, status) {
@@ -15,14 +15,16 @@ $(document).ready(function () {
     // creates a frame that holds the info for a product.
     function createFrameDiv(element) {
         $("#productContainer").append(
-            `<div id="product${element.productId}" class="row">
-                <div class="col-2 border d-flex justify-content-center">Image
+            `<div class=" d-flex row pb-2">
+                <div class="col-2 border justify-content-center">
+                    <img class="img-fluid" src="/images/Empty.png" alt="${element.pictureUrl} picture"/>
                 </div>
-                <div class="col-3">
+                <div class="col-3 border">
+                    <div class="d-flex flex-column h-100">
                     <div class="row border justify-content-center">
-                        <p>${element.name}</p>
+                        <a href="/product?id=${element.productId}">${element.name}</a>
                     </div>
-                    <div class="row border justify-content-center">
+                    <div class="row border justify-content-center flex-grow-1">
                         <p>${element.info}</p>
                     </div>
                     <div class="row border">
@@ -30,13 +32,18 @@ $(document).ready(function () {
                             <div class="row border justify-content-center"><p>Stock: ${element.stock}</p></div>
                         </div>
                         <div class="col">
-                            <div class="row border justify-content-center"><p>${element.cost}</p></div>
+                            <div class="row border justify-content-center"><p>$${parseFloat(element.cost).toFixed(2)}</p></div>
                         </div>
                     </div>
+                     
                 </div>
              </div> 
-             </br>`
-        )
+            `
+        );
+
+        $(`product${element.productId}`).on('click', function () {
+            console.log(this.id);
+        });
 
     }
 });

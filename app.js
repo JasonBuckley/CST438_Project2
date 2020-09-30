d9fe00b5ad9933a3f649a9ba34a028c7eee83f9c
Debbie Meda <54830275+bitmonst3r@users.noreply.github.com>
2020-09-30 15:39:00 -0700

    Started order page

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var productRouter = require("./routes/API/product");
 var homeRouter = require("./routes/home");
+var orderRouter = require("./routes/order");
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", homeRouter);
 app.use("/users", usersRouter);
 app.use("/product", productRouter);
+app.use("/order", orderRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

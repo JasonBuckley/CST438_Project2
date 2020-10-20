5939e6d11afac8fd7abcc054478edf608b79b903
Brian <bbaird@csumb.edu>
2020-10-20 14:55:52 -0700

    Clean create account. No address

--- public/javascripts/create_acct.js
+++ public/javascripts/create_acct.js
@@ -6,7 +6,7 @@ function signUp() {
     user.set("username", "my name");
     user.set("password", "my pass");
     //user.set("address", "my address");
-    //user.set("email", "email@example.com");
+    user.set("email", "email@example.com");
 
     user.signUp().then(function (user) {
         console.log('User created successful with name: ' + user.get("username") + ' and email: ' + user.get("email"));

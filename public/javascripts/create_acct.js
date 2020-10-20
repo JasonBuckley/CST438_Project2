285bc80c0e9fe99903208dc103160a519394dd2d
Brian <bbaird@csumb.edu>
2020-10-20 13:34:12 -0700

    cleaned up creatte account

--- public/javascripts/create_acct.js
+++ public/javascripts/create_acct.js
@@ -5,8 +5,8 @@ function signUp() {
     var user = new Parse.User();
     user.set("username", "my name");
     user.set("password", "my pass");
-    user.set("address", "my address");
-    user.set("email", "email@example.com");
+    //user.set("address", "my address");
+    //user.set("email", "email@example.com");
 
     user.signUp().then(function (user) {
         console.log('User created successful with name: ' + user.get("username") + ' and email: ' + user.get("email"));
